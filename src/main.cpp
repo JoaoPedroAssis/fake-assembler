c02f776aa692bf019e115c8904aa39225a68c0c2
joaopedroassis <joaopedroassisdossantos@gmail.com>
2020-09-28 11:44:01 -0300

    get command line args and split filename

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <string.h>
+#include "../include/Assembler.h"
+
+using namespace std;
 
 int main(int argc, char const *argv[]) {
     
-    std::cout << "Hello World!" << std::endl;
+    if (argc != 3) {
+        cout << "Incorrect number of arguments. Given: " << argc << ", Expected 3" << endl;
+        return 0;
+    }
+
+    string op, filepath;
+
+    op = argv[1];
+    filepath = argv[2];
+
+    // Splitting the filename from filepath
+    string file = filepath.substr(filepath.find_last_of("/") + 1);
+    string filename = file.substr(0, file.find_last_of('.'));
+
     return 0;
 }
