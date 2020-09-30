e6f87e6fbe93ef5a4a445ab38e3dbaaeb516e186
joaopedroassis <joaopedroassisdossantos@gmail.com>
2020-09-30 14:46:37 -0300

    classify the operation to be performed

--- src/main.cpp
+++ src/main.cpp
@@ -11,16 +11,30 @@ int main(int argc, char const *argv[]) {
         return 0;
     }
 
-    string op, programFilepath;
+    string flag, programFilepath;
 
-    op = argv[1];
+    flag = argv[1];
     programFilepath = argv[2];
 
+    int op;
+    if (flag == "-p") {
+        op = 0;
+    } else if ( flag == "-o") {
+        op = 1;
+    } else if (flag == "-po" || flag == "-op") {
+        op = 2;
+    }
     
 
     Assembler *assembler = new Assembler(programFilepath);
 
-    assembler->assemble(1);
+    try {
+        assembler->assemble(op);
+    } 
+    catch(const std::exception& e) {
+        std::cerr << e.what() << '\n';
+    }
+    
 
     return 0;
 }
