1cbd979e3c2ac8f3765925b3d91a2a8beef4fa74
joaopedroassis <joaopedroassisdossantos@gmail.com>
2020-10-10 19:56:49 -0300

    catch and print errors in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string.h>
 #include "../include/Assembler.h"
+#include "../include/Error.h"
 
 using namespace std;
 
@@ -23,18 +24,31 @@ int main(int argc, char const *argv[]) {
         op = 1;
     } else if (flag == "-po" || flag == "-op") {
         op = 2;
+    } else {
+        op = -1;
     }
-    
 
     Assembler *assembler = new Assembler(programFilepath);
+
+    // Errors e;
 
     try {
         assembler->assemble(op);
     } 
-    catch(const std::exception& e) {
+    catch(const invalid_argument& e) {
         std::cerr << e.what() << '\n';
-    }
-    
+        Errors::printErrors();
 
+        return 0;
+    }
+    catch(string& e) {
+        cout << e << endl;
+        return 0;
+    }
+
+    assembler->printAssembledFile();
+
+    // TODO resolver as labels duplas
+    // Validar argumentos das instruções
     return 0;
 }
